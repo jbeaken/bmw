551e24196e4be36afad61876f2a052b3353c4365
Chips <chips@chips.com>
2018-12-11 20:08:01 +0000

    Update build.sh and bmw-3.0 rename

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git pull
 mvn -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
 
 rm -rf /opt/tomcat/bmw/*
-cp -r target/bmw-boot-0.0.1-SNAPSHOT/. /opt/tomcat/bmw/
+cp -r target/bmw-3.0/. /opt/tomcat/bmw/
 
 # systemctl restart tomcat
 
