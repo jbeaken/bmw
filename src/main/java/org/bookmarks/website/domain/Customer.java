209764ff55de60565438cef44ed3327bbe6348c8
hal <hal@hal.com>
2018-12-12 22:31:23 +0000

    Adding fullName to jsonignore

--- src/main/java/org/bookmarks/website/domain/Customer.java
+++ src/main/java/org/bookmarks/website/domain/Customer.java
@@ -21,7 +21,7 @@ import org.hibernate.annotations.Type;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@JsonIgnoreProperties(value = { "hasBeenConsumed", "pendingConsumption" })
+@JsonIgnoreProperties(value = { "hasBeenConsumed", "pendingConsumption", "fullName" })
 public class Customer extends AbstractEntity {
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "customer", orphanRemoval = true, fetch = FetchType.EAGER)
